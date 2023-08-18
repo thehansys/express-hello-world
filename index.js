19c94b203e08d643b50806639c4bd6a4f5b66a7f
Jan Havlena <hanshavlena@gmail.com>
2023-08-18 10:37:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(Number(process.env.PORT), function () {
-  console.log('Listening on port' + process.env.PORT)
-})
+// app.listen(Number(process.env.PORT), function () {
+//   console.log('Listening on port' + process.env.PORT)
+// })
+
+export default app
